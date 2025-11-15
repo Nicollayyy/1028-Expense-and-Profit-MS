25b60140ff01e6f0debd7f9d775bc6568747ee96
Baron Moster <baronmoster1@gmail.com>
2025-11-15 12:52:49 +0800

    Added complete products SQL insert data

---
+++ products_inserts.sql
@@ -0,0 +1,112 @@
+INSERT INTO products (name, category, price) VALUES
+('Green Berry Fruit Soda', 'Soda Drinks', 60.00),
+('Mango Berry Fruit Soda', 'Soda Drinks', 60.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Chao Fan', 'Chao Fan', 70.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Add-on: Siomai', 'Chao Fan Add-ons', 5.00),
+('Add-on: Shanghai', 'Chao Fan Add-ons', 5.00),
+('Add-on: Longganisa', 'Chao Fan Add-ons', 10.00),
+('Add-on: Crispy Patty', 'Chao Fan Add-ons', 10.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Chao Fan with Premium Frappe', 'Chao Fan Bundle', 150.00),
+('Chao Fan with Fruit Tea', 'Chao Fan Bundle', 100.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Wintermelon Milk Tea (L)', 'Milk Tea', 38.00),
+('Dark Chocolate Milk Tea (L)', 'Milk Tea', 38.00),
+('Okinawa Milk Tea (L)', 'Milk Tea', 38.00),
+('Salted Caramel Milk Tea (L)', 'Milk Tea', 38.00),
+('Taro Milk Tea (L)', 'Milk Tea', 38.00),
+('Red Velvet Milk Tea (L)', 'Milk Tea', 38.00),
+('Matcha Milk Tea (L)', 'Milk Tea', 38.00),
+('Cookies & Cream Milk Tea (L)', 'Milk Tea', 38.00),
+('Choco Strawberry Milk Tea (L)', 'Milk Tea', 38.00);
+
+
+INSERT INTO products (name, category, price) VALUES
+('Wintermelon Cheesecake', 'Cheesecake Series', 53.00),
+('Okinawa Cheesecake', 'Cheesecake Series', 53.00),
+('Dark Chocolate Cheesecake', 'Cheesecake Series', 53.00),
+('Matcha Cheesecake', 'Cheesecake Series', 53.00),
+('Red Velvet Cheesecake', 'Cheesecake Series', 53.00),
+('Double Oreo Cheesecake', 'Cheesecake Series', 53.00),
+('Choco Strawberry Cheesecake', 'Cheesecake Series', 53.00),
+('Salted Caramel Cheesecake', 'Cheesecake Series', 53.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Dark Caramel Frappe', 'Regular Frappe', 55.00),
+('Dark Mocha Frappe', 'Regular Frappe', 55.00),
+('Java Chip Frappe', 'Regular Frappe', 55.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Matcha Frappe', 'Regular Frappe', 55.00),
+('Triple Chocolate Frappe', 'Regular Frappe', 55.00),
+('Dark Choco Berry Frappe', 'Regular Frappe', 55.00),
+('Strawberries and Cream Frappe', 'Regular Frappe', 55.00),
+('Blueberries and Cream Frappe', 'Regular Frappe', 55.00),
+('Mix Berries Frappe', 'Regular Frappe', 55.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Dark Choco Lava', 'Premium Frappe', 88.00),
+('Red Velvet Cream Cheese', 'Premium Frappe', 88.00),
+('Kopi Caramel', 'Premium Frappe', 88.00),
+('Dark Forest', 'Premium Frappe', 88.00),
+('Mango Cheesecake', 'Premium Frappe', 88.00),
+('Lava Cheesecake', 'Premium Frappe', 88.00),
+('Strawberry Cheesecake', 'Premium Frappe', 88.00),
+('Oreo Cheesecake', 'Premium Frappe', 88.00),
+('White Choco Mocha', 'Premium Frappe', 88.00),
+('Dark Choco Creamcheese', 'Premium Frappe', 88.00),
+('Ube Cream Cheese', 'Premium Frappe', 88.00),
+('Mint Chocolate Cream', 'Premium Frappe', 88.00),
+('Almond Matcha', 'Premium Frappe', 88.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Lychee Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Strawberry Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Green Apple Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Lemon Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Blueberry Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Kiwi Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Mango Fruit Tea (L)', 'Fruit Tea', 45.00),
+('Passion Fruit Tea (L)', 'Fruit Tea', 45.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Strawberry and Milk', 'Milk Series', 65.00),
+('Mango and Milk', 'Milk Series', 65.00),
+('Blueberries and Milk', 'Milk Series', 65.00),
+('Ube Matcha Latte', 'Milk Series', 65.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Iced Americano', 'Iced Coffee', 60.00),
+('Spanish Latte', 'Iced Coffee', 60.00),
+('White Chocolate', 'Iced Coffee', 60.00),
+('Mocha Latte', 'Iced Coffee', 60.00),
+('Caramel Macchiato', 'Iced Coffee', 60.00),
+('White Mocha', 'Iced Coffee', 60.00),
+('French Vanilla', 'Iced Coffee', 60.00),
+('Salted Caramel Latte', 'Iced Coffee', 60.00),
+('Dirty Matcha', 'Iced Coffee', 60.00),
+('Peppermint Mocha', 'Iced Coffee', 60.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Caramel Macchiato (Hot)', 'Hot Drinks', 45.00),
+('Salted Caramel Latte (Hot)', 'Hot Drinks', 45.00),
+('Spanish Latte (Hot)', 'Hot Drinks', 45.00),
+('White Chocolate (Hot)', 'Hot Drinks', 45.00),
+('Mocha Latte (Hot)', 'Hot Drinks', 45.00),
+('French Vanilla (Hot)', 'Hot Drinks', 45.00),
+('White Mocha (Hot)', 'Hot Drinks', 45.00),
+('Hot Chocolate', 'Hot Drinks', 45.00),
+('Hot Americano', 'Hot Drinks', 45.00);
+
+INSERT INTO products (name, category, price) VALUES
+('Add-on: Pearl', 'Add-ons', 10.00),
+('Add-on: Nata', 'Add-ons', 10.00),
+('Add-on: Crushed Oreo', 'Add-ons', 10.00),
+('Add-on: Extra Coffee Shot', 'Add-ons', 15.00),
+('Add-on: Cheesecake', 'Add-ons', 15.00);
